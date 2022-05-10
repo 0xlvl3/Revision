ec5a17dc8b5a48397ea7e5320d42e392c1f19165
0xlvl3 <cryptochoccy@gmail.com>
2022-05-10 20:31:25 +1000

    updated with level 9

--- Program.cs
+++ Program.cs
@@ -20,9 +20,62 @@ Console.WriteLine($"\nYou will deploy your attack too row:{row} column:{column}"
 //changing the text back to white for the next revision
 White();
 
+PressToContinue();
+Clear();
 
+//Reivison 2 intro
+Revision2();
 
+//gets user inputs
+int x = GetUserInt("Please enter a x value: ");
+int y = GetUserInt("Please enter a y value: ");
 
+//if statement that gives us our location of enemy
+Blue();
+if (x < 0 && y > 0)
+{
+     Console.WriteLine("\nThe enemy is NW!");
+}
+else if (x == 0 && y > 0)
+{
+    Console.WriteLine("\nThe enemy is N!");
+}
+else if (x > 0 && y > 0)
+{
+    Console.WriteLine("\nThe enemy is NE!");
+}
+else if (x < 0 && y == 0)
+{
+    Console.WriteLine("\nThe enemy is W!");
+}
+else if (x == 0 && y == 0)
+{
+    Console.WriteLine("\nThe enemy is near!");
+}
+else if (x > 0 && y == 0)
+{
+    Console.WriteLine("\nThe enemy is E!");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("\nThe enemy is SW!");
+}
+else if (x == 0 && x > 0)
+{
+    Console.WriteLine("\nThe enemy is S!");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("\nThe enemy is SE!");
+}
+else
+{
+    Console.WriteLine("\nError");
+}
+White();
+
+PressToContinue();
+Clear();
 
 
 
@@ -48,7 +101,7 @@ void Revision1()
     White();
 }
 
-
+//Method used to get row variable in first revision
 int getRow(int row)
 {
     while (true)
@@ -60,6 +113,35 @@ int getRow(int row)
     }
 }
 
+//Method press any key to continue so console just doesn't skip to the next chapter without letting the user see what they just entered
+void PressToContinue()
+{
+    Console.Write("\nPress any key to continue....");
+    Console.ReadKey(false);
+}
+
+//Method will clear console
+void Clear()
+{
+    Console.Clear();
+}
+
+//Method to get user int, can add string to the method
+int GetUserInt(string text)
+{
+    Console.Write($"\n{text}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+    
+}
+
+//method just goes to next line
+void jumpLine()
+{
+    Console.WriteLine("\n");
+}
+
+//Method used to get column variable in first revision
 int getColumn(int column)
 {
     while (true)
@@ -69,4 +151,13 @@ int getColumn(int column)
         if (column <= 0 && column >= 9) Console.WriteLine("This integer is not in range please enter between 1 - 8");
         if (column >= 1 && column <= 8) return column;
     }
+}
+
+//Title for level 9 revision
+void Revision2()
+{
+    string level = "Level 9";
+    Console.WriteLine($"{level,10}");
+    Console.WriteLine("Watchtower");
+    Console.WriteLine("-----------");
 }
